681fe5b01c1496fe2239e3e4c32a7751fe3cbbea
Nguyễn Bảo <baolongkiem8@gmail.com>
2025-06-09 03:48:51 +0700

    zX
    ZX

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -61,11 +61,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.UseAuthorization();
